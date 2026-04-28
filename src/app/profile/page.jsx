2ea0a247fcc757697af583f776e2a942a4de2b90
umayer10-AI <mdumayer10@gmail.com>
2026-04-28 16:06:38 +0600

    add

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { UpdateModal } from "@/component/UpdateModal";
 import { authClient } from "@/lib/auth-client";
 import { Avatar } from "@heroui/react";
 import React from "react";
@@ -10,17 +11,20 @@ const page = () => {
   return (
     <div>
       <div className="flex justify-center mt-10">
-        <div className="border text-center space-y-3 p-4 rounded-2xl shadow">
-          <Avatar className="mx-auto h-20 w-20">
-            <Avatar.Image
-              alt="John Doe"
-              referrerPolicy="no-referrer"
-              src={user?.image}
-            />
-            <Avatar.Fallback>{user?.name?.charAt(0)}</Avatar.Fallback>
-          </Avatar>
-          <h2 className="font-semibold text-2xl">{user?.name}</h2>
-          <h2>{user?.email}</h2>
+        <div className="border w-90 text-center space-y-3 py-6 rounded-2xl shadow">
+            <Avatar className="mx-auto h-20 w-20">
+                <Avatar.Image
+                alt="John Doe"
+                referrerPolicy="no-referrer"
+                src={user?.image}
+                />
+                <Avatar.Fallback>{user?.name?.charAt(0)}</Avatar.Fallback>
+            </Avatar>
+            <h2 className="font-semibold text-2xl">{user?.name}</h2>
+            <h2 className="text-muted">{user?.email}</h2>
+
+            <UpdateModal></UpdateModal>
+            
         </div>
       </div>
     </div>
