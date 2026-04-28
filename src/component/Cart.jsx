1d7769440fd6f9a592c13f68182f1a0e047c9075
umayer10-AI <mdumayer10@gmail.com>
2026-04-28 10:13:43 +0600

    add

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -9,7 +9,7 @@ const Cart = async () => {
     console.log(data)
 
     return (
-        <div>
+        <div className='max-w-[90%] mx-auto'>
             <h2 className='text-2xl font-bold mt-7'>Top Generations</h2>
             <CartItems></CartItems>
         </div>
--- src/component/CartItems.jsx
+++ src/component/CartItems.jsx
@@ -5,7 +5,11 @@ const CartItems = () => {
     return (
         <div>
             <div>
-                <Image src={"https://i.pinimg.com/1200x/04/6a/ef/046aef3824fc35cd1ad0bff0f253619a.jpg"} alt='image'></Image>
+                <Image height={100} width={100} className='w-full h-60 object-cover rounded-2xl' src={"https://i.pinimg.com/1200x/04/6a/ef/046aef3824fc35cd1ad0bff0f253619a.jpg"} alt='image'></Image>
+            </div>
+            <h2 className='font-semibold'>Realistic Portrait AI</h2>
+            <div>
+                
             </div>
         </div>
     );
