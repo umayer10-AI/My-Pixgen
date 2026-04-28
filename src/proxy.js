d1a30d6b48b954a7b5e4fe7ac818e558b0a40c2d
umayer10-AI <mdumayer10@gmail.com>
2026-04-28 18:03:29 +0600

    add

--- src/proxy.js
+++ src/proxy.js
@@ -1,9 +1,17 @@
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
 
-export function proxy(request) {
-  return NextResponse.redirect(new URL('/home', request.url))
+export async function proxy(request) {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    console.log(session)
+
+    return NextResponse.redirect(new URL('/', request.url))
 }
 
 export const config = {
-  matcher: ['/price'],
+  matcher: ['/profile'],
 }
