4383a23c5362a9aaa397e59e93dd09bd9fa1e7f3
umayer10-AI <mdumayer10@gmail.com>
2026-04-28 16:21:56 +0600

    add

--- src/component/UpdateModal.jsx
+++ src/component/UpdateModal.jsx
@@ -1,55 +1,61 @@
 "use client";
 
+import { authClient } from "@/lib/auth-client";
 import {Envelope} from "@gravity-ui/icons";
 import {Button, Input, Label, Modal, Surface, TextField} from "@heroui/react";
+import { BiEdit } from "react-icons/bi";
+import { FaRegUser } from "react-icons/fa";
 
 export function UpdateModal() {
+
+    const handleClick = async (e) => {
+        e.preventDefault()
+
+        const name = e.target.name.value
+        const url = e.target.url.value
+        console.log(name,url)
+
+        await authClient.updateUser({
+            name,
+            url,
+        })
+    }
+
   return (
     <Modal>
-      <Button variant="secondary">Open Contact Form</Button>
+      <Button variant="secondary"><BiEdit></BiEdit> Update Profile</Button>
       <Modal.Backdrop>
         <Modal.Container placement="auto">
           <Modal.Dialog className="sm:max-w-md">
             <Modal.CloseTrigger />
             <Modal.Header>
               <Modal.Icon className="bg-accent-soft text-accent-soft-foreground">
-                <Envelope className="size-5" />
+                <FaRegUser  className="size-5"/>
               </Modal.Icon>
-              <Modal.Heading>Contact Us</Modal.Heading>
-              
+              <Modal.Heading>Update User</Modal.Heading>
+
             </Modal.Header>
             <Modal.Body className="p-6">
               <Surface variant="default">
-                <form className="flex flex-col gap-4">
+                <form onSubmit={handleClick} className="flex flex-col gap-4">
                   <TextField className="w-full" name="name" type="text">
                     <Label>Name</Label>
                     <Input placeholder="Enter your name" />
                   </TextField>
-                  <TextField className="w-full" name="email" type="email">
-                    <Label>Email</Label>
-                    <Input placeholder="Enter your email" />
+                  <TextField className="w-full" name="url" type="text">
+                    <Label>Image URL</Label>
+                    <Input placeholder="Image URL" />
                   </TextField>
-                  <TextField className="w-full" name="phone" type="tel">
-                    <Label>Phone</Label>
-                    <Input placeholder="Enter your phone number" />
-                  </TextField>
-                  <TextField className="w-full" name="company">
-                    <Label>Company</Label>
-                    <Input placeholder="Enter your company name" />
-                  </TextField>
-                  <TextField className="w-full" name="message">
-                    <Label>Message</Label>
-                    <Input placeholder="Enter your message" />
-                  </TextField>
-                </form>
-              </Surface>
-            </Modal.Body>
+                  
             <Modal.Footer>
               <Button slot="close" variant="secondary">
                 Cancel
               </Button>
-              <Button slot="close">Send Message</Button>
+              <Button type="submit" slot="close">Save</Button>
             </Modal.Footer>
+                </form>
+              </Surface>
+            </Modal.Body>
           </Modal.Dialog>
         </Modal.Container>
       </Modal.Backdrop>
