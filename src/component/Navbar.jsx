6f6a4444f094188e2822889a11ed236101f899e5
umayer10-AI <mdumayer10@gmail.com>
2026-04-28 08:41:37 +0600

    add

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,11 +2,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import img from '../assets/image.png'
+import { Button } from "@heroui/react";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+
+    const p = usePathname()
+
   return (
-    <div className="border-b px-2">
-      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
+    <div className="border-b">
+      <div className="w-[90%] mx-auto">
+        <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
         <div className="flex gap-2 items-center">
           <Image
             src={img}
@@ -16,35 +22,36 @@ const Navbar = () => {
             height={30}
             className="object-cover h-auto w-auto"
           />
-          <h3 className="font-black text-lg">pixgen.</h3>
+          <h3 className="font-black text-lg">pixgen</h3>
         </div>
 
-        <ul className="flex items-center gap-5 text-sm">
+        <ul className="flex items-center gap-5 font-semibold">
           <li>
-            <Link href={"/"}>Home</Link>
+            <Link href={"/"} className={p==='/' ? 'border-b border-b-green-600 text-green-600':""}>Home</Link>
           </li>
           <li>
-            <Link href={"/all-photos"}>All Photos</Link>
+            <Link href={"/all-photos"} className={p==='/all-photos' ? 'border-b border-b-green-600 text-green-600':""}>All Photos</Link>
           </li>
           <li>
-            <Link href={"/pricing"}>Pricing</Link>
+            <Link href={"/pricing"} className={p==='/pricing' ? 'border-b border-b-green-600 text-green-600':""}>Pricing</Link>
           </li>
           <li>
-            <Link href={"/profile"}>Profile</Link>
+            <Link href={"/profile"} className={p==='/profile' ? 'border-b border-b-green-600 text-green-600':""}>Profile</Link>
           </li>
         </ul>
 
         <div className="flex gap-4">
-          <ul className="flex items-center  text-sm">
-            <li>
+          <ul className="flex items-center gap-2">
+            <Button>
               <Link href={"/signup"}>SignUp</Link>
-            </li>
-            <li>
+            </Button>
+            <Button>
               <Link href={"/signin"}>SignIn</Link>
-            </li>
+            </Button>
           </ul>
         </div>
       </nav>
+      </div>
     </div>
   );
 };
